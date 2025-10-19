01d8848c8ffeb0e5cebf34be9d50d1c87232e4d9
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-10-19 10:18:17 +0200

    poprawki main

--- lab2/tensorflow_operations.py
+++ lab2/tensorflow_operations.py
@@ -4,12 +4,7 @@ import sys
 import numpy as np
 import tensorflow as tf
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--degree", type=int, default=30)
-parser.add_argument("-n", type=int)
-parser.add_argument("-A", type=int, nargs="+", required=True)
-parser.add_argument("-b", type=int, nargs="+", required=True)
-args = parser.parse_args()
+
 
 
 # Zadanie 1/2.
@@ -22,9 +17,6 @@ def rotate(points, theta):
     return tf.matmul(rotation_matrix, points)
 
 
-print(rotate(tf.constant([[2.0, 0.0]], shape=(2,1)), np.deg2rad(args.degree, dtype=np.float32)))
-
-
 def solve_linear_system(A, b):
     det = tf.linalg.det(A)
     if det == 0:
@@ -35,16 +27,22 @@ def solve_linear_system(A, b):
     return x
 
 
-A = tf.constant(args.A, shape=(args.n, args.n), dtype=tf.float32)
-b = tf.constant(args.b, shape=(args.n, 1), dtype=tf.float32)
-solution = solve_linear_system(A, b)
-solution = solution.numpy().flatten()
-print(solution)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--degree", type=int, default=30)
+    parser.add_argument("-n", type=int)
+    parser.add_argument("-A", type=int, nargs="+", required=True)
+    parser.add_argument("-b", type=int, nargs="+", required=True)
+    args = parser.parse_args()
 
-def main() -> int:
-    return 0
+    print(rotate(tf.constant([[2.0, 0.0]], shape=(2, 1)), np.deg2rad(args.degree, dtype=np.float32)))
 
+    A = tf.constant(args.A, shape=(args.n, args.n), dtype=tf.float32)
+    b = tf.constant(args.b, shape=(args.n, 1), dtype=tf.float32)
+    solution = solve_linear_system(A, b)
+    solution = solution.numpy().flatten()
+    print(solution)
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(0)
