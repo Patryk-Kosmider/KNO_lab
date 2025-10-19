208d41502a32e7f30730f4acd7e2ac0cdc57bb05
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-10-19 10:08:55 +0200

    zad4/5

--- lab2/tensorflow_operations.py
+++ lab2/tensorflow_operations.py
@@ -6,6 +6,9 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--degree", type=int, default=30)
+parser.add_argument("-n", type=int)
+parser.add_argument("-A", type=int, nargs="+", required=True)
+parser.add_argument("-b", type=int, nargs="+", required=True)
 args = parser.parse_args()
 
 # Zadanie 1/2.
@@ -15,12 +18,23 @@ def rotate(points, theta):
         [tf.cos(theta), -tf.sin(theta), tf.sin(theta), tf.cos(theta)]
     )
     rotation_matrix = tf.reshape(rotation_matrix, (2, 2))
-    print(rotation_matrix)
     return tf.matmul(rotation_matrix, points)
 
-print(rotate(tf.constant([2.0, 0.0], shape=(2,1)), np.deg2rad(args.degree, dtype=np.float32)))
+#print(rotate(tf.constant([[2.0, 0.0]], shape=(2,1)), np.deg2rad(args.degree, dtype=np.float32)))
 
-# Zadanie 3
+
+def solve_linear_system(A, b):
+    det = tf.linalg.det(A)
+    if det == 0:
+        raise ValueError("Układ nie ma rozwiązań, lub ma nieskończenie wiele / wyznacznik równy 0")
+    x = tf.linalg.solve(A, b)
+    return x
+A = tf.constant(args.A,shape=(args.n, args.n), dtype=tf.float32)
+b = tf.constant(args.b,shape=(args.n,1), dtype=tf.float32)
+solution = solve_linear_system(A, b)
+solution = solution.numpy().flatten()
+print(solution)
+
 
 def main() -> int:
     return 0
